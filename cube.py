2203ad25703f4a8f612a127cf979af828582a7b6
Vincent Wong <TheBrutux168@gmail.com>
2020-01-19 12:08:31 +1100

    Fix bug in Cube rotation methods

--- cube.py
+++ cube.py
@@ -27,16 +27,27 @@ class Cube:
     def _generate_face(self, colour: str, size: int):
         return [[colour for i in range(size)] for j in range(size)]
 
-    def rotate(self):
-        pass
-
+    def rotate(self, face: str, double: bool, prime: bool):
+        if double:
+            self._face_rotate(face, False)
+            self._adjacent_face_swap(face, False)
+            self._face_rotate(face, False)
+            self._adjacent_face_swap(face, False)
+        elif prime:
+            for i in range(3):
+                self._face_rotate(face, prime)
+                self._adjacent_face_swap(face, prime)
+        else:
+            self._face_rotate(face, prime)
+            self._adjacent_face_swap(face, prime)
+            
     def _face_rotate(self, face: str, counter_clockwise: bool):
         if not counter_clockwise:
             self.faces[face] = \
                     [list(row) for row in zip(*self.faces[face][::-1])]
         else:
             self.faces[face] = \
-                    [list(row)[::-1] for row in zip(*(self.faces[face]))]
+                    [list(row) for row in zip(*(self.faces[face]))]
 
     def _adjacent_face_swap(self, face: str, counter_clockwise: bool):
         if face == "U":
@@ -58,32 +69,30 @@ class Cube:
         elif face == "R":
             l = [self._transpose(l) for l in 
                                 [self.faces["U"], self.faces["B"], self.faces["D"], self.faces["F"]]]
-            r = l[1:] + l[:1] if counter_clockwise else l[-1:] + l[:-1]
+            r = [l[0][-1][::-1], l[1][0][::-1], l[2][-1], l[3][-1]]
 
-            l[0][-1], l[1][-1], l[2][-1], l[3][-1] \
-                = [t[-1] for t in r]
+            l[0][-1], l[1][0], l[2][-1], l[3][-1] = r[-1:] + r[:-1]
 
             for i, face in enumerate(["U", "B", "D", "F"]):
                 self.faces[face] = self._transpose(l[i])
 
         elif face == "L":
             l = [self._transpose(l) for l in 
-                                [self.faces["U"], self.faces["B"], self.faces["D"], self.faces["F"]]]
-            r = l[1:] + l[:1] if not counter_clockwise else l[-1:] + l[:-1]
+                                [self.faces["U"], self.faces["F"], self.faces["D"], self.faces["B"]]]
+            r = [l[0][0], l[1][0], l[2][0][::-1], l[3][-1][::-1]]
 
-            l[0][0], l[1][0], l[2][0], l[3][0] \
-                = [t[-1] for t in r]
+            l[0][0], l[1][0], l[2][0], l[3][-1] = r[-1:] + r[:-1]
 
-            for i, face in enumerate(["U", "B", "D", "F"]):
+            for i, face in enumerate(["U", "F", "D", "B"]):
                 self.faces[face] = self._transpose(l[i])
-        
+
         elif face == "F":
             l = [self.faces["U"], self._transpose(self.faces["R"]), 
                  self.faces["D"], self._transpose(self.faces["L"])]
-            r = l[1:] + l[:1] if counter_clockwise else l[-1:] + l[:-1]
+            r = [l[0][-1], l[1][0][::-1], l[2][0], l[3][-1][::-1]]
 
             l[0][-1], l[1][0], l[2][0], l[3][-1] \
-                = [t[-1] for t in r]
+                = r[-1:] + r[:-1]
 
             self.faces["U"][-1] = l[0][-1]
             self.faces["R"] = self._transpose(l[1])
@@ -93,10 +102,9 @@ class Cube:
         elif face == "B":
             l = [self.faces["U"], self._transpose(self.faces["R"]), 
                  self.faces["D"], self._transpose(self.faces["L"])]
-            r = l[1:] + l[:1] if not counter_clockwise else l[-1:] + l[:-1]
+            r = [l[0][0][::-1], l[1][-1], l[2][-1][::-1], l[3][0]]
 
-            l[0][0], l[1][-1], l[2][-1], l[3][0] \
-                = [t[-1] for t in r]
+            l[0][0], l[1][-1], l[2][-1], l[3][0] = r[1:] + r[:1]
 
             self.faces["U"][0] = l[0][0]
             self.faces["R"] = self._transpose(l[1])
