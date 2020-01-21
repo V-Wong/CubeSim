0b4bcef9acfce24ae045fb56b47d261fd633d2e5
Vincent Wong <TheBrutux168@gmail.com>
2020-01-21 19:42:04 +1100

    Add timing to Statistics class

--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 from typing import Type, Callable
+import time
 
 from openpyxl import Workbook
 
@@ -6,6 +7,7 @@ from cube import Cube
 from scramble_generator import gen_scramble
 from solver import generate_solution
 from scramble_parser import moves_to_scramble
+
 
 class Statistics:
     def __init__(self, cube_class: Type[Cube], scrambler: Callable, solver: Callable):
@@ -17,15 +19,21 @@ class Statistics:
         self.worksheet = self.workbook.active
 
     def gen_headers(self):
-        self.worksheet.append(["Scramble", "Length Of Solution", "Solution"])
+        self.worksheet.append(["Time Taken To Generate Solution", "Scramble", "Length Of Solution", "Solution"])
 
     def generate_statistics(self, solves: int):
         for _ in range(solves):
             cur_cube = self.cube_class(3)
             scramble = self.scrambler()
-            cur_cube.do_moves(scramble)
+            cur_cube.do_moves(scramble, save_history=False)
+
+            start_time = time.clock()
             solution = self.solver(cur_cube)
-            self.worksheet.append([scramble, len(solution), 
+            end_time = time.clock()
+
+            solution_time = end_time - start_time
+
+            self.worksheet.append([solution_time, scramble, len(solution), 
                                    moves_to_scramble(solution)])
 
         self.workbook.save("cube_statistics.xlsx")
@@ -34,4 +42,4 @@ class Statistics:
 if __name__ == "__main__":
     stats = Statistics(Cube, gen_scramble, generate_solution)
     stats.gen_headers()
-    stats.generate_statistics(100)
+    stats.generate_statistics(1000)
