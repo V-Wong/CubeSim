cea404ce50f0618e820365e7d2a46f12ebd0ba13
Vincent Wong <TheBrutux168@gmail.com>
2020-01-21 19:21:58 +1100

    Add moves to scramble converter

--- scramble_parser.py
+++ scramble_parser.py
@@ -12,6 +12,22 @@ def scramble_to_moves(scramble: str) -> List[Tuple[int, bool, bool]]:
     return moves
 
 
+def moves_to_scramble(moves: List[Tuple[int, bool, bool]]) -> str:
+    scramble = []
+
+    for move in moves:
+        cur_move = move[0]
+        
+        if move[2]:
+            cur_move += "2"
+        elif move[1]:
+            cur_move += "'"
+
+        scramble.append(cur_move)
+    
+    return " ".join(scramble)
+
+
 if __name__ == "__main__":
     scramble = "L U2 D B' R2 U2 F R B2 U2 R2 U R2 U2 F2 D R2 D F2"
 
