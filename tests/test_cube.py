5d4e934552d7ab2c30e65e0207f75e5e71c94732
Vincent Wong <vincent@vwong.dev>
2021-01-15 15:41:09 +1100

    Update tests

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -22,7 +22,7 @@ def test_solve_arbitrary_scrambles():
     for _ in range(1):
         cube = Cube(3)
         scramble = gen_scramble()
-        cube.do_moves(scramble)
+        cube.do_moves(scramble, save_history=False)
         solution = generate_solution(cube)
         cube.do_moves(solution)
         assert cube.is_solved()
